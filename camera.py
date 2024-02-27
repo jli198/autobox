b744072e251680cbbaa503215eeb1a51a75d3b44
jli198 <jli198@stevens.edu>
2024-02-27 13:36:39 -0500

    test'

--- camera.py
+++ camera.py
@@ -3,31 +3,39 @@ import datetime as dt
 import sys
 import subprocess
 import os
+from rekognition_detect import *
 
 IMAGE_NAME = dt.datetime.now().strftime('%m%d%Y%H%M%S')+ '.jpg'
 BUCKET = "s3://autoboximages/"
-SRC_DIR = '/home/admin/Documents/' + IMAGE_NAME
-DEST = BUCKET + "images/"
+SRC_DIR = '/home/admin/' + IMAGE_NAME # path may be wrong now
+DEST = BUCKET + "images/" # need to remake images folder in bucket
 CURRENT_DATE = dt.datetime.now().strftime('%m/%d/%Y %H:%M:%S')
 
 def camera(file):
-  cmd = 'rpicam-jpeg'
-  args = '-o'
-  temp = subprocess.Popen([cmd, args, file], stdout = subprocess.PIPE)
-  output = str(temp.communicate())
-  output = output.split("\n")
-  output = output[0].split('\\')
+	cmd = 'rpicam-jpeg'
+	args = '-o'
+	temp = subprocess.Popen([cmd, args, file], stdout = subprocess.PIPE)
 
-  res = []
-  for line in output:
-    res.append(line)
-  print('camera results')
-  print('\n'.join(res[len(res) - 3:len(res) - 1]))
-  return res
+	output = str(temp.communicate())
+	output = output.split("\n")
+	output = output[0].split('\\')
+
+	res = []
+	for line in output:
+		res.append(line)
+	print('camera results')
+	print('\n'.join(res[len(res) - 3:len(res) - 1]))
+	return res
 
 camera(IMAGE_NAME)
 
 CMD = "s3cmd put --acl-public %s %s" % (SRC_DIR, BUCKET)
 print(CMD)
 subprocess.call(CMD, shell=True)
-os.remove('/home/admin/Documents/'+IMAGE_NAME)
+time.sleep(5)
+detect_text(BUCKET[5:-1], IMAGE_NAME, IMAGE_NAME[:-4])
+time.sleep(1)
+detect_labels(BUCKET[5:-1], IMAGE_NAME, IMAGE_NAME[:-4])
+os.remove('/home/admin/'+IMAGE_NAME)
+# os.remove('/home/admin/'+IMAGE_NAME[:-4]+'_Text.json')
+# os.remove('/home/admin/'+IMAGE_NAME[:-4]+'_Labels.json')
