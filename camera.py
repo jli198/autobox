4b99157ceb520f90e109e3b59e226ac0ed409253
mdigeron <97755080+mdigeron@users.noreply.github.com>
2024-04-18 09:16:45 -0400

    fixed incorrect variables in send_mms()

--- camera.py
+++ camera.py
@@ -71,7 +71,8 @@ while True:
 	time.sleep(1)
 	word_location(IMAGE_NAME, parsed_text[1])
 	message = "Detected Labels: " + parsed_labels + "\n" + "Detected Text: " + parsed_text[0]
-	send_mms(number1, message1, SRC_DIR, "image", IMAGE_NAME[-4:], carrier)
+	send_mms(number1, message, SRC_DIR, "image", IMAGE_NAME[-4:], carrier1)
+	send_mms(number2, message, SRC_DIR, "image", IMAGE_NAME[-4:], carrier2)
 	os.remove('/home/admin/autobox/'+IMAGE_NAME)
 	os.remove('/home/admin/autobox/'+IMAGE_NAME[:-4]+'_Text.json')
 	os.remove('/home/admin/autobox/'+IMAGE_NAME[:-4]+'_Labels.json')
